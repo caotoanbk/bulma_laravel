bca4f31ae50c3b1ad9f4d3030e09ef4323d6a49b
Toan Cao <caotoanbk@gmail.com>
2019-10-20 20:45:48 +0700

    first commit

---
+++ resources/views/includes/nav/main.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar is-info">
+  <div class="container">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="112" height="28">
+       </a>
+      <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+
+    <div id="navbarExampleTransparentExample" class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link" href="https://bulma.io/documentation/overview/start/">
+              Hi, toancv!
+            </a>
+            <div class="navbar-dropdown is-boxed is-right">
+              <a class="navbar-item p-t-4 p-b-4" href="#">
+                <i class="fa fa-cogs" aria-hidden="true">&nbsp;Settings</i>
+              </a>
+              <a class="navbar-item" href="#">
+                <i class="fa fa-user">&nbsp;User profile</i>
+              </a>
+              <hr class="navbar-divider"></hr>
+              <a class="navbar-item" href="#">
+                <i class="fa fa-power-off" aria-hidden="true">&nbsp;Logout</i>
+              </a>
+           </div>
+          </div>
+        </div>
+      </div>
+  </div>
+</nav>
---
+++ resources/views/layouts/manage.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>ggdfgdfgd</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+        @include ('includes.nav.main')
+        <section class="main-content columns is-fullheight">
+            @include('includes.nav.manage')
+
+          <div class="container column is-10 has-background-light">
+            @yield('content')
+          </div>
+          
+        </section>
+    </div>
+</body>
+</html>
